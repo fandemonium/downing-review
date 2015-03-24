372cc508343923d7e51a4ca1e6f60e047969624d
Adina Howe <adina.chuang@gmail.com>
2015-03-24 08:57:33 -0500

    added cumulative scripts

---
+++ parse2.py
@@ -0,0 +1,36 @@
+import glob, string
+
+d = {}
+f = glob.glob("*txt")
+for x in f:
+    for line in open(x):
+        line = line.translate(string.maketrans("",""), string.punctuation)
+        for word in line.rstrip().split(' '):
+            word = word.lower()
+            print word
+            if d.has_key(word):
+                d[word][x] = d[word].get(x,0) + 1
+            else:
+                d[word] = {}
+                d[word][x] = 1
+
+list_journals = []
+for x in f:
+    list_journals.append(x)
+l = sorted(list_journals)
+
+fp = open('summary_cum.tsv', 'w')
+for x in l:
+    fp.write('\t%s' % x)
+fp.write('\n')
+
+
+for k in d:
+    fp.write('%s\t' % k)
+    for j in l:
+        if d[k].has_key(j):
+            fp.write('%s\t' % d[k][j])
+        else:
+            fp.write('0\t')
+    fp.write('\n')
+
