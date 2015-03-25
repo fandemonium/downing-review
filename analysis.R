9cab3ed4f49c604e1b0c65771e097627df502bf0
Fan Yang <snisiarc@gmail.com>
2015-03-25 14:44:56 -0500

    Deleted journal review guideline files manually

--- analysis.R
+++ analysis.R
@@ -15,10 +15,11 @@ abund <- read.delim(sep='\t', file="./summary.tsv",header=TRUE, strip.white=TRUE
 abund <- read.delim(sep='\t', file="./summary_cum.tsv",header=TRUE, strip.white=TRUE, row.names=1)
 abund$X <- NULL
 
-meta <- read.delim(sep='\t', file="./meta.tsv", header=TRUE, strip.white=TRUE, row.names=1)
+meta <- read.delim(sep='\t', file="./meta.txt", header=TRUE, strip.white=TRUE, row.names=1)
+meta$Notes<-NULL
 meta$journal_name = rownames(meta)
+#bmc.txt are not unique and causing issues when trying to set it as 
 rownames(meta) = meta$Text.file.name
-
 
 abundance_data_norm_matrix <- as.matrix(abund)
 abundance <- otu_table(abundance_data_norm_matrix, taxa_are_rows=TRUE)
@@ -40,4 +41,3 @@ p + geom_bar(stat="identity", colour="black")+theme_bw()+theme(strip.text.x=elem
 #just for fun, who added
 p = ggplot(f, aes_string(x="OTU", y="SUM", fill="Who.added"))
 p + geom_bar(stat="identity", colour="black")+theme_bw()+theme(strip.text.x=element_text(angle=90), axis.text.x=element_text(angle=90, size=5))+labs(y="# of observations")
-f
